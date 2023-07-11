20f37c549eed60843df89b6e905e2c375abad706
OduOduE <theoduemmanuel@gmail.com>
2023-07-11 17:14:50 +0000

    doneButNotDusted

---
+++ 0x02-minimum_operations/0-main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 0
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 1
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 5
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 7
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 9
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 24
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 29
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 48
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 99
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
